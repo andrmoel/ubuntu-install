b5ea7652d14f2cf2e88c8d01c4f9d37037ed5124
Andreas Moeller <andrmoel@gmail.com>
2019-11-18 16:02:56 +0100

    Initial commit

---
+++ install.bash
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+sudo apt update
+sudo apt upgrade -y
+
+sudo apt install -y \
+    apt-transport-https \
+    ca-certificates \
+    gnupg-agent \
+    software-properties-common \
+    exfat-fuse \
+    exfat-utils \
+    curl \
+    snapd \
+    vim \
+    terminator \
+    php \
+    git \
+    slack
+
+# Spotify
+sudo snap install spotify
+
+# yarn
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt update
+sudo apt install -y yarn
+
+# docker
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt update
+sudo apt install -y docker-ce docker-compose
+sudo groupadd docker
+sudo usermod -aG docker $USER
+
+# Google Chrome
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+sudo apt update
+sudo apt install -y google-chrome-stable
+
+# PHPStorm
+sudo snap install phptorm --clasic
+
+# Clean up
+sudo apt autoremove -y
+sudo apt autoclean -y
